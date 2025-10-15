c5f00b950fe251e14c633faa5b3b9522801e8e4d
molixmoli <i.molina@lefebvre.es>
2025-10-15 17:16:30 +0200

    Cambio de los test

--- test_prueba.py
+++ test_prueba.py
@@ -4,9 +4,9 @@ import allure
 @allure.suite("Pruebas de ejemplo")
 @allure.title("Test con resultados variados")
 def test_1():
-    assert False
+    assert True
 
 @allure.suite("Pruebas de ejemplo")
 @allure.title("Test con resultados variados")
 def test_2():
-    assert True
+    assert False
