6e0b3e4c26faf0d4658a002068a25d4396b5f97a
molixmoli <i.molina@lefebvre.es>
2025-10-15 09:21:48 +0200

    Con allure

--- test_prueba.py
+++ test_prueba.py
@@ -1,8 +1,12 @@
 import pytest
+import allure
 
-
+@allure.suite("Pruebas de ejemplo")
+@allure.title("Test con resultados variados")
 def test_1():
     assert True
-    
+
+@allure.suite("Pruebas de ejemplo")
+@allure.title("Test con resultados variados")
 def test_2():
     assert False
